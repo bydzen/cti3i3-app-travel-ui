5873090b6815febd8c049318ba8fa3798976c860
bydzen <svnteentthones@gmail.com>
2022-06-19 21:57:00 +0700

    add backend endpoints

---
+++ locavel-backend/index.js
@@ -0,0 +1,92 @@
+// Require module
+var express = require('express'),
+    http = require('http'),
+    bodyParser = require('body-parser');
+
+// MySQL required
+var connection = require('./config/database');
+
+// The app express
+var app = express();
+
+// Middleware
+
+// Parsing the body
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Parsing the JSON
+app.use(express.json());
+app.use(bodyParser.json());
+
+// App port
+app.set('port', process.env.PORT || 3000);
+
+// Trust proxy
+app.set('trust proxy', true);
+
+// Connect and check the database
+connection.connect();
+connection.query('SELECT 1 + 1 AS solution', function (error, results) {
+    if (error) throw error;
+    console.log(
+        'The solution is: ',
+        results[0].solution,
+        ' database connetcted successfully!'
+    );
+});
+
+// Root path
+app.get('/', function (_req, res) {
+    res.json({
+        message: 'Welcome to LocaVel API',
+    });
+});
+
+// Post wisata
+app.post('/wisata', function (req, res) {
+    var data = {
+        nama: req.body.nama,
+        tempat: req.body.tempat,
+        harga: req.body.harga,
+        konten: req.body.konten,
+        rating: req.body.rating,
+        gambar: req.body.gambar,
+    };
+    connection.query(
+        'INSERT INTO wisata SET ?',
+        data,
+        function (error, results) {
+            if (error) throw error;
+            res.json({
+                message: 'Wisata berhasil ditambahkan',
+                data: data,
+            });
+        }
+    );
+});
+
+// Get wisata
+app.get('/wisata', function (req, res) {
+    connection.query('SELECT * FROM wisata', function (error, results) {
+        if (error) throw error;
+        res.json(results);
+    });
+});
+
+// Get wisata by id
+app.get('/wisata/:id', function (req, res) {
+    connection.query(
+        'SELECT * FROM wisata WHERE id = ?',
+        [req.params.id],
+        function (error, results) {
+            if (error) throw error;
+            res.json(results);
+        }
+    );
+});
+
+// Creating the server
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Listening to ' + app.get('port'));
+});
