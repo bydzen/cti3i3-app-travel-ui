2a46db44ec70be2928e0ee860e72b5e0a9562c6c
bydzen <svnteentthones@gmail.com>
2022-06-22 09:34:57 +0700

    Use port 4000

--- locavel-backend/index.js
+++ locavel-backend/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // App port
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 
 // Trust proxy
 app.set('trust proxy', true);
