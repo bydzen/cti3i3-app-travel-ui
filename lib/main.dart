9e701afe92add7bf4a1dc3cacc7bb33dfb7e64ce
bydzen <svnteentthones@gmail.com>
2022-06-24 11:07:51 +0700

    add example of usage api

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
-import 'package:flutter/material.dart';
-import 'package:LocaVel/const.dart';
-import 'package:LocaVel/screens/home.dart';
+import 'dart:convert';
 
-void main() {
-  runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:LocaVel/screens/home.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+
+Future<void> main() async {
+  await dotenv.load();
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   _MyAppState createState() => _MyAppState();
 }
@@ -14,10 +21,45 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: appTitle,
-      home: const Home(),
+      title: "LocaVel",
+      home: Home(),
     );
+  }
+}
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  const Album({
+    required this.userId,
+    required this.id,
+    required this.title,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+    );
+  }
+}
+
+Future<Album> fetchAlbum() async {
+  final response =
+      await http.get(Uri.parse('https://locavel.herokuapp.com/wisata/1'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
   }
 }
